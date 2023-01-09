4c62ea5ddb4daff17940687d3cf7db96040de470
Fernando Reyes <hapoweiser@gmail.com>
2023-01-09 00:16:45 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,45 @@
+#
+#   Segundo prototipo para el control de volume para Onos
+#
+
+from flask import Flask, render_template, request, flash
+import threading,time,soco,subprocess,os
+
+device = soco.discovery.any_soco()
+
+exit_event = threading.Event()
+
+global volume
+volume = 35
+
+def AdjustVolume():  
+    global volume
+
+    while not exit_event.is_set():
+        device.volume = volume
+        time.sleep(0.01)
+
+app = Flask(__name__)
+app.secret_key = "SvaW4F$FGGG<>:T"
+
+@app.route("/")
+def index():
+    return render_template("index.html", volume=volume, device_name=device.player_name, device_ip=device.ip_address)
+
+@app.route("/save", methods=["POST","GET"])
+def save():
+    global volume
+    volume = int(request.form['volume_input'])
+
+    return render_template("index.html", volume=str(volume), device_name=device.player_name, device_ip=device.ip_address)
+
+def main():
+    volume_thread = threading.Thread(target=AdjustVolume)
+    volume_thread.start()
+
+    app.run(host="0.0.0.0", port=80)
+
+    exit_event.set()
+
+if __name__ == "__main__":
+    main()
