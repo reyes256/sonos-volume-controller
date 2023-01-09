8ce0eadebd9f4025792cd40333b90e5a91864994
Fernando Reyes <hapoweiser@gmail.com>
2023-01-09 11:26:24 -0700

    se agregaron batch scripts

---
+++ scripts/Setup.bat
@@ -0,0 +1,6 @@
+@echo off
+
+pip install soco
+pip install flask
+
+cd C:\ && git clone https://github.com/japotitlan/sonos-volume-controller.git
---
+++ scripts/SonosVolume.bat
@@ -0,0 +1,3 @@
+@echo off
+
+python C:\sonos-volume-controller\app.py
