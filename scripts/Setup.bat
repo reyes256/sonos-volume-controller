ba43da7ad27525e63146bc4b566d02dbc967ae7d
Fernando Reyes <hapoweiser@gmail.com>
2023-03-13 12:26:17 -0700

    updated user name

--- scripts/Setup.bat
+++ scripts/Setup.bat
@@ -3,6 +3,6 @@
 pip install soco
 pip install flask
 
-cd C:\ && git clone https://github.com/japotitlan/sonos-volume-controller.git
+cd C:\ && git clone https://github.com/reyes256/sonos-volume-controller.git
 
 cd C:\sonos-volume-controller\ && git pull
