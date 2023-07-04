a7496113cf5927557cd49100944b34cba957f514
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-07-04 00:40:28 -0700

    removed unused .bat files
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- scripts/Setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-pip install soco
-pip install flask
-
-cd C:\ && git clone https://github.com/reyes256/sonos-volume-controller.git
-
-cd C:\sonos-volume-controller\ && git pull
--- scripts/SonosVolume.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-mode con cols=85 lines=20
-
-start http://localhost
-
-@REM python ../app.py
-python C:\sonos-volume-controller\app.py
